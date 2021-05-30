00145fb22635f8e08a1c0d98a3ced60cf5f0d07e
FelipeO22 <andresfelipe51@hotmail.es>
2021-05-29 22:00:57 -0500

    Controlador/servicio factura

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FacturasController } from './facturas/facturas/facturas.controller';
+import { FacturasService } from './facturas/facturas/facturas.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+    type: 'mongodb',
+    url:
+      'mongodb+srv://<admin>:<password>@chnirt-graphql-apollo-vg0hq.mongodb.net/nest?retryWrites=true&w=majority',
+    entities: [join(__dirname, '**/**.entity{.ts,.js}')],
+    synchronize: true,
+    useNewUrlParser: true,
+    logging: true,
+  })],
+  controllers: [AppController, FacturasController],
+  providers: [AppService, FacturasService],
 })
 export class AppModule {}
